7103b64c18b6963d89f2591bafaa2cdb03e83ac7
Travis Dillard <prohormonepro@gmail.com>
2026-03-26 00:16:16 +0000

    revert: Home.jsx to 2b1b6c0

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -300,13 +300,14 @@ export default function Home() {
             </p>
           </div>
           <div className="flex flex-col gap-2 flex-shrink-0 ml-4">
-            <Link
-            <Link
-              to="/consultation"
+            <a
+              href="https://prohormonepro.com"
+              target="_blank"
+              rel="noopener noreferrer"
               className="prohp-btn-primary text-xs"
             >
               Book a Consultation
-            </Link>
+            </a>
             <Link
               to="/r/general"
               className="text-[10px] text-[var(--text-muted)] hover:text-[var(--prohp-blue)] transition-colors text-right"
