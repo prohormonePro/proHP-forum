80bb1308cbac3820bf5ae92f276cffa6ee99e006
Travis Dillard <prohormonepro@gmail.com>
2026-04-02 21:14:13 +0000

    fix(2162): add api.patch to hooks/api.js

--- frontend/src/hooks/api.js
+++ frontend/src/hooks/api.js
@@ -29,4 +29,5 @@ export const api = {
   post: (path, body) => apiFetch(path, { method: 'POST', body: JSON.stringify(body) }),
   put: (path, body) => apiFetch(path, { method: 'PUT', body: JSON.stringify(body) }),
   del: (path) => apiFetch(path, { method: 'DELETE' }),
+  patch: (path, body) => apiFetch(path, { method: 'PATCH', body: JSON.stringify(body) }),
 };
